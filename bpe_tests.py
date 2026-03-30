a1da997c5f6af26d0eaec0460612768de6a78d9a
Max Bernstein <max@bernsteinbear.com>
2026-03-30 13:42:43 -0400

    Check we compressed

--- bpe_tests.py
+++ bpe_tests.py
@@ -10,6 +10,7 @@ class BPeTests(unittest.TestCase):
         self.assertEqual(tokens, {0: 'T', 1: 'h', 2: 'e', 3: ' ', 4: 'f', 5: 'o', 6: 'x', 7: ' ', 8: 'j', 9: 'u', 10: 'm', 11: 'p', 12: 'e', 13: 'd', 14: ' ', 15: 'o', 16: 'v', 17: 'e', 18: 'r', 19: ' ', 20: 't', 21: 'h', 22: 'e', 23: ' ', 24: 'f', 25: 'e', 26: 'n', 27: 'c', 28: 'e', 29: '!', 30: 'he'})
         self.assertEqual(token_ids, {v: k for k, v in tokens.items()})
         self.assertEqual(steps, [('h', 'e')])
+        self.assertLess(len(encoded), len(text))
 
     def test_train_two_rounds(self):
         text = "The fox jumped over the fence!"
@@ -24,6 +25,7 @@ class BPeTests(unittest.TestCase):
                                   31: 'he '})
         self.assertEqual(token_ids, {v: k for k, v in tokens.items()})
         self.assertEqual(steps, [('h', 'e'), ('he', ' ')])
+        self.assertLess(len(encoded), len(text))
 
 if __name__ == '__main__':
     unittest.main()
